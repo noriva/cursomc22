9f77c67427265cb2a009d668dac8202d1daa8ca0
noriva <jrmarson@bol.com.br>
2019-05-16 23:19:50 -0300

    criando operação de instanciação

--- src/main/java/com/marson/cursomc22/Cursomc22Application.java
+++ src/main/java/com/marson/cursomc22/Cursomc22Application.java
@@ -1,13 +1,34 @@
 package com.marson.cursomc22;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Cursomc22Application {
+import com.marson.cursomc22.domain.Categoria;
+import com.marson.cursomc22.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class Cursomc22Application implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository ;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Cursomc22Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null , "Informática");
+		Categoria cat2 = new Categoria(null , "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
